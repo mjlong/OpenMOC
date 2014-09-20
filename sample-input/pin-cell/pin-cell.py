8f5f65a5d7c0a897ec2d893280757d0253baddc4
Jilang Miao 缪佶朗@office <jlmiao@mit.edu>
2014-09-20 16:36:27 -0400

    disable plot again

--- sample-input/pin-cell/pin-cell.py
+++ sample-input/pin-cell/pin-cell.py
@@ -110,7 +110,7 @@ track_generator.generateTracks()
 solver = CPUSolver(geometry, track_generator)
 solver.setNumThreads(num_threads)
 solver.setSourceConvergenceThreshold(tolerance)
-solver.convergeSource(max_iters)
+solver.convergeSource(40)#(max_iters)
 solver.printTimerReport()
 
 
@@ -120,11 +120,11 @@ solver.printTimerReport()
 
 log.py_printf('NORMAL', 'Plotting data...')
 
-plotter.plot_tracks(track_generator)
-plotter.plot_segments(track_generator)
-plotter.plot_materials(geometry, gridsize=500)
-plotter.plot_cells(geometry, gridsize=500)
-plotter.plot_flat_source_regions(geometry, gridsize=500)
-plotter.plot_fluxes(geometry, solver, energy_groups=[1,2,3,4,5,6,7])
+#plotter.plot_tracks(track_generator)
+#plotter.plot_segments(track_generator)
+#plotter.plot_materials(geometry, gridsize=500)
+#plotter.plot_cells(geometry, gridsize=500)
+#plotter.plot_flat_source_regions(geometry, gridsize=500)
+#plotter.plot_fluxes(geometry, solver, energy_groups=[1,2,3,4,5,6,7])
 
 log.py_printf('TITLE', 'Finished')
